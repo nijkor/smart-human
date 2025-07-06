f40ec5d341c0bfffc7b4f44b024550e39b9eeb53
Alexey Nikitin <165425781+nijkor@users.noreply.github.com>
2025-07-06 17:26:13 +0300

    commit

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 // Замените на нужный вам адрес изображения
 const NEW_IMAGE_URL = "https://i.ibb.co/YFXZVwF7/image.png";
 
-// Функция для замены всех картинок
 function replaceImages() {
     const images = document.querySelectorAll("img");
     images.forEach(img => {
@@ -12,9 +11,7 @@ function replaceImages() {
     });
 }
 
-// Заменить изображения сразу
 replaceImages();
 
-// Пытаться заменять изображения при динамических изменениях DOM (например, подгрузка новых картинок)
 const observer = new MutationObserver(replaceImages);
 observer.observe(document.body, { childList: true, subtree: true });
